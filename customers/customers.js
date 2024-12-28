883730b144a87be36305cb9fd16cf3b294e6d193
Isaiah Vickers <isaiah.vickers@outlook.com>
2024-12-28 16:59:55 -0500

    build customer routes

--- customers/customers.js
+++ customers/customers.js
@@ -20,14 +20,40 @@ app.post("/customers", async (req, res) => {
     const customer = await new Customer(customerData).save();
     res.status(201).send(customer);
   } catch (error) {
-    res.sendStatus(500);
-    throw new Error(error);
+    return res.status(500).send(error);
   }
 });
 
 app.get("/customers", async (req, res) => {
   const customers = await Customer.find();
   res.send(customers);
+});
+
+app.get("/customers/:id", async (req, res) => {
+  try {
+    const customer = await Customer.findById(req.params.id);
+    if (!customer) {
+      return res.sendStatus(404);
+    }
+
+    res.send(customer);
+  } catch (error) {
+    res.sendStatus(500);
+    throw new Error(error);
+  }
+});
+
+app.delete("/customers/:id", async (req, res) => {
+  try {
+    const customer = await Customer.findByIdAndDelete(req.params.id);
+    if (!customer) {
+      return res.sendStatus(404);
+    }
+
+    res.send("Customer deleted successfully");
+  } catch (error) {
+    res.status(500).send(error);
+  }
 });
 
 module.exports = app;
